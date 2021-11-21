f42f0776164586d3cad46061cb7c8feba220d155
opasnostinet <veselovalova@icloud.com>
2021-11-21 15:16:17 +0300

    top level components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,17 @@
-<script context="module" lang="ts">
-	export const prerender = true;
+<script>
+	import Cta from '$lib/CTA.svelte';
+	import Footer from '$lib/Footer.svelte';
+	import Hero from '$lib/hero.svelte';
+	import Navbar from '$lib/Navbar.svelte';
+	import Pricing from '$lib/Pricing.svelte';
+	import Testimonials from '$lib/Testimonials.svelte';
+	import WhyUs from '$lib/WhyUs.svelte';
 </script>
 
-<script lang="ts">
-	import Counter from '$lib/Counter.svelte';
-</script>
-
-<svelte:head>
-	<title>Home</title>
-</svelte:head>
-
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<Navbar/>
+<Hero/>
+<WhyUs/>
+<Pricing/>
+<Testimonials/>
+<Cta/>
+<Footer/>
