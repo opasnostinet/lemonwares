acf8df355cbc4306d2cd4970638f94edabc126b9
opasnostinet <veselovalova@icloud.com>
2021-12-12 18:15:33 +0300

    toggler

--- src/lib/Pricing.svelte
+++ src/lib/Pricing.svelte
@@ -1,13 +1,26 @@
-<h2>Ready to get started with 
-  Lemon Wares?</h2>
-  <label for=""></label>
-  <input type="checkbox">
-  <ul>
-    <li>Basic</li>
-    <li>Premium</li>
-  </ul>
-  <ul>
-    <li>Starter</li>
-    <li>Standart</li>
-    <li>Suprem</li>
-  </ul>
+<script>
+	import Toggler from '$lib/Toggler.svelte';
+</script>
+
+<section class="wrapper">
+	<h2>Ready to get started with Lemon Wares?</h2>
+	<Toggler option1="Monthly" option2="Yearly" small="30% discount" />
+	<ul>
+		<li>Basic</li>
+		<li>Premium</li>
+	</ul>
+	<ul>
+		<li>Starter</li>
+		<li>Standart</li>
+		<li>Suprem</li>
+	</ul>
+</section>
+
+<style>
+	.wrapper {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+</style>
---
+++ src/lib/Toggler.svelte
@@ -0,0 +1,73 @@
+<script>
+	export let option1;
+	export let option2;
+	export let small;
+</script>
+
+<label class="switch">
+	<input type="checkbox" />
+  {option1}
+	<div class="slider round" />
+  {option2}
+	<small>{small}</small>
+</label>
+
+<style>
+	.switch {
+		display: flex;
+		align-items: center;
+		gap: 15px;
+	}
+
+	.switch input {
+		opacity: 0;
+		width: 0;
+		height: 0;
+		position: absolute;
+	}
+
+	.slider {
+		width: 93px;
+		height: 50px;
+		cursor: pointer;
+		background-color: #b0000052;
+		-webkit-transition: 0.4s;
+		transition: 0.4s;
+		position: relative;
+	}
+
+	.slider:before {
+		position: absolute;
+		content: '';
+		height: 30px;
+		width: 30px;
+		left: 11px;
+		bottom: 10px;
+		background-color: white;
+		-webkit-transition: 0.4s;
+		transition: 0.4s background-color, 0.4s transform;
+	}
+
+	input:checked + .slider {
+		background-color: #b00000;
+	}
+
+	input:checked + .slider:before {
+		transform: translateX(41px);
+	}
+
+	/* Rounded sliders */
+	.slider.round {
+		border-radius: 34px;
+	}
+
+	.slider.round:before {
+		border-radius: 73px;
+	}
+
+	small {
+		padding: 5px;
+		background-color: #ffe87a;
+		border-radius: 8px;
+	}
+</style>
