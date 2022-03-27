8e259642c64f897b22d1286e5f0a3b1cbc5bce7d
opasnostinet <veselovalova@icloud.com>
2022-03-27 17:31:32 +0300

    Fix import register and general hero layout

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,12 +1,39 @@
-<ul>
-  <li>Hosting</li>
-  <li>Domain</li>
-  <li>SEO</li>
-  <li>Email</li>
-</ul>
+<section>
+	<div>
+		<ul>
+			<li>Hosting</li>
+			<li>Domain</li>
+			<li>SEO</li>
+			<li>Email</li>
+		</ul>
 
-<h1>Premium Web Hosting for Your Website</h1>
-<p>Blazing fast web hosting for individuals and businesses of all sizes backed by 24x7x365 Support.</p>
-<button>Create an Account</button>
-<button>Choose your plan</button>
-<img src="/logo/person-with-notebook.png" alt="person-with-notebook">
+		<h1>Premium Web Hosting for Your Website</h1>
+		<p>
+			Blazing fast web hosting for individuals and businesses of all sizes backed by 24x7x365
+			Support.
+		</p>
+		<button>Create an Account</button>
+		<button>Choose your plan</button>
+	</div>
+	<img src="/logo/person-with-notebook.png" alt="person-with-notebook" />
+</section>
+
+<style>
+	ul {
+		display: flex;
+		list-style-type: none;
+    margin: 0;
+	}
+
+	img {
+    width: 35%;
+    height: 100%;
+	}
+
+  section{
+    display: flex;
+    margin: 50px 110px;
+    gap: 50px;
+  }
+
+</style>
