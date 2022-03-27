6b31ad05b39b76e7b057dc121308509c489b6fc2
opasnostinet <veselovalova@icloud.com>
2022-03-27 17:32:50 +0300

    Fix invalid import

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Cta from '$lib/CTA.svelte';
 	import Footer from '$lib/Footer.svelte';
-	import Hero from '$lib/hero.svelte';
+	import Hero from '$lib/Hero.svelte';
 	import Navbar from '$lib/Navbar.svelte';
 	import Pricing from '$lib/Pricing.svelte';
 	import Testimonials from '$lib/Testimonials.svelte';
