fbb900bf9ceca9576c2125060fd4c06e3061d774
opasnostinet <veselovalova@icloud.com>
2022-04-23 19:11:11 +0300

    layout CTA component

--- src/lib/CTA.svelte
+++ src/lib/CTA.svelte
@@ -1,5 +1,68 @@
-<h2>We're here to make your website awesome.</h2>
-<a href="#"><img src="icon/whats-up.svg" alt="whatsapp">Get  in touch with us</a>
-<div></div>
-<div></div>
-<div></div>
+<script>
+	import Button from './Button.svelte';
+</script>
+
+<section>
+	<h2>We're here to make your website awesome.</h2>
+	<a href="#">
+		<Button>
+			<img class="whatsapp-icon" src="icon/whats-up.svg" alt="whatsapp" />
+			<span>Get in touch with us</span>
+		</Button>
+	</a>
+	<img class="triangle triangle-1" src="icon/triangle.svg" alt="" />
+	<img class="triangle triangle-2" src="icon/triangle.svg" alt="" />
+	<img class="triangle triangle-3" src="icon/triangle.svg" alt="" />
+</section>
+
+<style>
+	section {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		align-items: center;
+		background-color: #f7fafe;
+		padding: 80px 90px;
+		position: relative;
+	}
+
+	h2 {
+		width: 80%;
+		margin-top: 0;
+		margin-bottom: 40px;
+		text-align: center;
+	}
+
+	.whatsapp-icon {
+		width: 25px;
+		height: 25px;
+	}
+
+	.triangle {
+		position: absolute;
+	}
+
+	.triangle-1 {
+		right: -10px;
+		bottom: 30px;
+	}
+
+	.triangle-2 {
+		left: -5px;
+		bottom: 70px;
+		transform: scaleX(-1);
+		width: 40px;
+		height: 40px;
+	}
+
+	.triangle-3 {
+		top: 0px;
+		left: 200px;
+		width: 40px;
+		height: 40px;
+	}
+	a {
+		text-decoration: none;
+		cursor: pointer;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,26 +9,29 @@
 	import WhyUs from '$lib/Why-us.svelte';
 </script>
 
-<Navbar/>
-<Hero/>
-<WhyUs/>
-<Pricing/>
-<div class="testimonials-wrapper"><Testimonials/></div>
-<div class="portfolio-wrapper"><Portfolio/></div>
-<Cta/>
-<Footer/>
+<Navbar />
+<Hero />
+<WhyUs />
+<Pricing />
+<div class="testimonials-wrapper"><Testimonials /></div>
+<div class="portfolio-wrapper"><Portfolio /></div>
+<div class="call-to-action-wrapper"><Cta /></div>
+<Footer />
 
 <style>
 	:global(*) {
 		outline: 1px dashed red;
 	}
-.testimonials-wrapper{
-	margin-left: 142px;
-	margin-right: 104px;
-	position: relative;
-}
-.portfolio-wrapper{
-margin: 120px;
+	.testimonials-wrapper {
+		margin-left: 142px;
+		margin-right: 104px;
+		position: relative;
+	}
+	.portfolio-wrapper {
+		margin: 120px;
+	}
 
-}
+	.call-to-action-wrapper {
+		margin: 80px;
+	}
 </style>
