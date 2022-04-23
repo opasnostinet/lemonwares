cc4a7ffb37aa537aaa9bc3655845e3e0c3ec8332
opasnostinet <veselovalova@icloud.com>
2022-04-23 19:10:22 +0300

    add button component

---
+++ src/lib/Button.svelte
@@ -0,0 +1,18 @@
+<button>
+	<slot />
+</button>
+
+<style>
+	button {
+		display: flex;
+		background-color: #b00000;
+		border-radius: 8px;
+		padding: 12px 16px;
+		align-items: center;
+		color: #fff;
+		gap: 8px;
+		line-height: 24px;
+		border: none;
+		cursor: inherit;
+	}
+</style>
