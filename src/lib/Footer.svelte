11170fa0e70f0a2c6e5c8ac2cea37d53c7ec740f
opasnostinet <veselovalova@icloud.com>
2022-05-06 20:10:11 +0300

    make layout footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,12 +1,12 @@
-<div>
-	<img src="logo/logo.png" alt="logo" />
-	<p>
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dolores perferendis doloremque odio
-		assumenda natus a molestias, expedita aliquam illo accusantium tenetur ad unde mollitia fugit
-		blanditiis porro, sequi delectus?
-	</p>
-</div>
-<div>
+<div class="footer">
+	<div>
+		<img class="logo" src="logo/logo.png" alt="logo" />
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dolores perferendis doloremque
+			odio assumenda natus a molestias, expedita aliquam illo accusantium tenetur ad unde mollitia
+			fugit blanditiis porro, sequi delectus?
+		</p>
+	</div>
 	<ul>
 		<li><a href="#"><b>Service</b> </a></li>
 		<li><a href="#">Domain</a></li>
@@ -14,8 +14,6 @@
 		<li><a href="#">Cloud Hosting</a></li>
 		<li><a href="#">Private Hosting</a></li>
 	</ul>
-</div>
-<div>
 	<ul>
 		<li><a href="#"><b>Hosting</b> </a></li>
 		<li><a href="#">Cheap Hosting</a></li>
@@ -23,22 +21,54 @@
 		<li><a href="#">Email Hosting</a></li>
 		<li><a href="#">Hosting Unlimited</a></li>
 	</ul>
-</div>
-<div>
 	<ul>
 		<li><a href="#"><b>Company</b> </a></li>
 		<li><a href="#">About</a></li>
 		<li><a href="#">Career</a></li>
 		<li><a href="#">Contact Us</a></li>
 	</ul>
-</div>
-<div>
 	<ul>
 		<li><a href="#"><b>Help</b></a></li>
 		<li><a href="#">FAQ</a></li>
 		<li><a href="#">Help Support</a></li>
 	</ul>
+	<div class="copyright">
+		Built by Jeremiah with <img src="icon/heart.svg" alt="love" /> in Lagos. Copyright 2021
+	</div>
 </div>
-<div>
-  <div>Built by Jeremiah with <img src="icon/heart.svg" alt="love"> in Lagos. Copyright 2021</div>
-</div>
+
+<style>
+	.footer {
+		background: #b00000;
+		padding: 60px 9% 80px;
+		gap: 96px 8%;
+		color: white;
+		display: grid;
+		grid-template-columns: 20% 1fr 1fr 1fr 1fr;
+	}
+
+	.copyright {
+		grid-column-end: span 5;
+	}
+
+	.copyright img {
+		width: 22px;
+		height: 20px;
+		vertical-align: text-bottom;
+	}
+
+	.logo{
+		filter: brightness(0) contrast(2) invert(1);
+	}
+
+	ul{
+		list-style-type: none;
+	}
+
+	a{
+		text-decoration: none;
+		color:#E6E6E6;
+
+
+	}
+</style>
