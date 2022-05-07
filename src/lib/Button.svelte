3d9e03adc8848fa7b30ce22518c16d5c052a6c02
opasnostinet <veselovalova@icloud.com>
2022-05-07 19:51:40 +0300

    add new type of button

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,4 +1,8 @@
-<button>
+<script>
+	export let secondary = false;
+</script>
+
+<button class:secondary>
 	<slot />
 </button>
 
@@ -14,5 +18,20 @@
 		line-height: 24px;
 		border: none;
 		cursor: inherit;
+	}
+
+	.secondary {
+		display: flex;
+		border-radius: 8px;
+    background-color: #fff;
+		padding: 12px 16px;
+		align-items: center;
+		color: #b00000;
+		gap: 8px;
+		line-height: 24px;
+		cursor: inherit;
+		border: 1px solid #000000;
+		box-sizing: border-box;
+		border-radius: 8px;
 	}
 </style>
