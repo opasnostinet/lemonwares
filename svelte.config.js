13608846dda24b8a6ca60fd8911dd5b474ded910
opasnostinet <veselovalova@icloud.com>
2023-10-16 22:15:36 +0400

    migrate sveltekit 3 to 4

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
-
-export default config;
