673c4bfbd8c32f9f3db3afa57a6d22bd678388a0
opasnostinet <veselovalova@icloud.com>
2023-10-16 22:15:36 +0400

    migrate sveltekit 3 to 4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
-	import Cta from '$lib/CTA.svelte';
-	import Footer from '$lib/Footer.svelte';
-	import Navbar from '$lib/Navbar.svelte';
-	import Hero from '$lib/Hero.svelte';
-	import Pricing from '$lib/Pricing.svelte';
-	import Testimonials from '$lib/Testimonials.svelte';
-	import Portfolio from '$lib/Portfolio.svelte';
-	import WhyUs from '$lib/Why-us.svelte';
+	import Cta from "$lib/CTA.svelte";
+	import Footer from "$lib/Footer.svelte";
+	import Navbar from "$lib/Navbar.svelte";
+	import Hero from "$lib/Hero.svelte";
+	import Pricing from "$lib/Pricing.svelte";
+	import Testimonials from "$lib/Testimonials.svelte";
+	import Portfolio from "$lib/Portfolio.svelte";
+	import WhyUs from "$lib/Why-us.svelte";
 </script>
 
 <Navbar />
@@ -19,10 +19,6 @@
 <Footer />
 
 <style>
-	:global(*) {
-		outline: 1px dashed red;
-	}
-
 	.testimonials-wrapper {
 		margin-left: 142px;
 		margin-right: 104px;
